76e2d58dad42d0f484df8eb2884c08a82eceb6d3
Andi Lanuza <andi@photokitchenfood.com>
2026-05-03 17:44:55 +0800

    Campaigns Bug Fix

--- campaigns-index.js
+++ campaigns-index.js
@@ -7,7 +7,7 @@ const CAMPAIGNS_INDEX = [
     "id": "260421-04 Mother_s Day",
     "name": "04 Mother's Day",
     "shoot_date": "2026-04-21",
-    "filename": "260421-04 Mother_s Day.js",
+    "filename": "260421-04-Mother_s-Day.js",
     "photo_count": 4,
     "added": "2026-04-27"
   }
